fc84a262926ac2213a4b79679e6cbad2b25132d8
Alejandro Segura <asegurpe@gmail.com>
2021-12-09 13:58:30 +0100

    I18n for iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cacapp/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:cacapp/routes/routes.dart';
 import 'package:cacapp/screen/home_screen.dart';
@@ -22,16 +23,18 @@ class CacApp extends StatelessWidget {
         return MaterialPageRoute(builder: (context) => const HomeScreen());
       },
       theme: theme,
-      localizationsDelegates: [
+      localizationsDelegates: const [
+        AppLocalizations.delegate, // Add this line
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: [
-        Locale('en', ''),
-        Locale('es', ''),
-        Locale('ca', ''),
+        Locale('en'),
+        Locale('es'),
+        Locale('ca'),
       ],
+      locale: Locale('ca'),
     );
   }
 }
