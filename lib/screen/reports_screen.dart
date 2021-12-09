9abefb12d460a0a66b4816508ed1fcf5f074ab1a
Alejandro Segura <asegurpe@gmail.com>
2021-12-09 12:07:50 +0100

    Testing with cards

--- lib/screen/reports_screen.dart
+++ lib/screen/reports_screen.dart
@@ -12,8 +12,11 @@ class ReportsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Reports'),
       ),
-      body: ListView(
-        children: _getReports(),
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.all(10),
+          children: _getReports(),
+        ),
       ),
     );
   }
@@ -29,17 +32,81 @@ class ReportsScreen extends StatelessWidget {
         picture: '',
         size: 0,
         smell: 0,
+      ),
+      Report(
+        cleaned: false,
+        confirmed: false,
+        consistency: 0,
+        description: '01/01/2022 00:00',
+        informer: '1341341',
+        picture:
+            'https://www.k9ofmine.com/wp-content/uploads/2020/09/dog-pooping-inside-850x520.jpg',
+        size: 0,
+        smell: 0,
+      ),
+      Report(
+        cleaned: false,
+        confirmed: false,
+        consistency: 0,
+        description: '01/01/2022 00:00',
+        informer: '1341341',
+        picture:
+            'https://www.k9ofmine.com/wp-content/uploads/2020/09/dog-pooping-inside-850x520.jpg',
+        size: 0,
+        smell: 0,
       )
     ];
 
     return reports.map((report) {
-      return ListTile(
-        title: Text(report.description),
-        subtitle: Text(report.informer),
-        leading: Image(image: AssetImage('assets/images/marker.png')),
-        trailing: Icon(Icons.keyboard_arrow_right),
-        onTap: () {},
-      );
+      if (report.picture.isEmpty) {
+        return Card(
+          child: Column(
+            children: [
+              ListTile(
+                title: Text(report.description),
+                subtitle: Text(report.informer),
+                leading: Image(image: AssetImage('assets/images/marker.png')),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {},
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(onPressed: () {}, child: Text('CONFIRM')),
+                  TextButton(onPressed: () {}, child: Text('DELETE')),
+                ],
+              ),
+            ],
+          ),
+        );
+      } else {
+        return Card(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(5.0),
+            child: Column(
+              children: [
+                FadeInImage(
+                  image: NetworkImage(report.picture),
+                  placeholder: AssetImage('assets/images/marker.png'),
+                  fadeInDuration: Duration(milliseconds: 200),
+                  height: 250,
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                    padding: EdgeInsets.all(8),
+                    child: Text(report.description)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(onPressed: () {}, child: Text('CONFIRM')),
+                    TextButton(onPressed: () {}, child: Text('DELETE')),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      }
     }).toList();
   }
 }
